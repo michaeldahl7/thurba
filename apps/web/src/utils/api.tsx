94e0eadccdb0a0cf170ab89510a07dd054b2e9de
Michael Dahl <michaeldahl7@gmail.com>
2024-08-03 16:11:33 -0700

    polish

--- apps/web/src/utils/api.tsx
+++ apps/web/src/utils/api.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { httpBatchLink } from "@trpc/client";
+import { httpBatchLink, loggerLink } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import superjson from "superjson";
 import type { AppRouter } from "@acme/api";
 export { type RouterInputs, type RouterOutputs } from "@acme/api";
@@ -10,9 +11,25 @@ export function TRPCProvider(props: { children: React.ReactNode }) {
   const queryClient = new QueryClient();
   const trpcClient = api.createClient({
     links: [
+      loggerLink({
+        enabled: (opts) =>
+          process.env.NODE_ENV === "development" ||
+          (opts.direction === "down" && opts.result instanceof Error),
+        colorMode: "ansi",
+      }),
       httpBatchLink({
         url: "http://localhost:3000/trpc",
         transformer: superjson,
+        headers() {
+          const headers = new Map<string, string>();
+          headers.set("x-trpc-source", "vite-react");
+
+          // const token = getToken();
+          const token = 1234;
+          if (token) headers.set("Authorization", `Bearer ${token}`);
+
+          return Object.fromEntries(headers);
+        },
       }),
     ],
   });
@@ -21,16 +38,8 @@ export function TRPCProvider(props: { children: React.ReactNode }) {
     <api.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
         {props.children}
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </api.Provider>
   );
-}
-
-// biome-ignore lint/complexity/noUselessLoneBlockStatements: <explanation>
-{
-  /* <api.Provider client={trpcClient} queryClient={queryClient}>
-<QueryClientProvider client={queryClient}>
-  {props.children}
-</QueryClientProvider>
-</api.Provider> */
 }
